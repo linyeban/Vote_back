d5d0b271ab2204dba29928e0837a985e2bc551f3
linyeban <2901109886@qq.com>
2017-01-05 15:00:20 +0800

    第一版本2

--- src/test/java/com/lin/test/MyTest1.java
+++ src/test/java/com/lin/test/MyTest1.java
@@ -4,13 +4,15 @@ import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
 import com.lin.Util.MyBatisUtil;
+import com.lin.dao.IAdminDao;
 import com.lin.dao.IUserDao;
+import com.lin.entity.Admin;
 import com.lin.entity.User;
 
 //测试Mybatis
 public class MyTest1 {
 	@Test
-	public void testaddmin() {
+	public void testuser() {
 		SqlSession sqlSession = MyBatisUtil.getSession();
 		IUserDao userD = sqlSession.getMapper(IUserDao.class);
 		User User1=new User();
@@ -18,6 +20,17 @@ public class MyTest1 {
 		User1.setPassword("1234");
 		User1.setAge(11);
 		userD.insert(User1);
+		sqlSession.commit();
+	}
+	@Test
+	public void testadmin() {
+		SqlSession sqlSession = MyBatisUtil.getSession();
+		IAdminDao adminD = sqlSession.getMapper(IAdminDao.class);
+		Admin admin1=new Admin();
+		admin1.setName("我是管理员");
+		admin1.setPassword("1234");
+		admin1.setLogintime("22222222");
+		adminD.addAdmin(admin1);
 		sqlSession.commit();
 	}
 		
